ec79ce590fc21869f1cfcbabb04057ff38c77e2d
Yajat Gupta <128760326+Thisisyajat@users.noreply.github.com>
2024-10-31 16:57:24 +0000

    feat: Add SQL integration programs for displaying records and importing data, along with a SQL exercise document

---
+++ New Files/School work/Year 2/Practicals/17_import_sql.py
@@ -0,0 +1,6 @@
+# This program integrates SQL and Python using the SQL module.
+# (C) Yajat Gupta
+
+import mysql.connector
+db = mysql.connector.connect(host = 'localhost', user = 'root', password = '', database = 'test1')
+print('SQL Database test1 online.')
